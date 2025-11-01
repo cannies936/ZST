a56da7a7524010da8552b4ab16c3f5b11f6505cc
cannies936 <volence02471@gmail.com>
2025-11-01 15:45:57 +0900

    resume.py の更新

--- command/voice/resume.py
+++ command/voice/resume.py
@@ -1,8 +1,8 @@
-@bot.tree.command(name="resume", description="一時停止中の曲を再開します。")
+@bot.tree.command(name="resume", description="一時停止中の曲を再開します")
 async def resume(interaction: discord.Interaction):
     vc = interaction.guild.voice_client
     if vc and vc.is_paused():
         vc.resume()
-        await interaction.response.send_message("▶ 再開しました。")
+        await interaction.response.send_message("▶ 再開しました")
     else:
-        await interaction.response.send_message("⚠ 一時停止中の曲がありません。")
+        await interaction.response.send_message("⚠ 一時停止中の曲がありません")
